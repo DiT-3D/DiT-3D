8a781393d3ad12a4eda6384087cf6a3175b35c0d
stoneMo <shentongmo@ShentongMos-MacBook-Pro-2.local>
2023-09-06 10:18:25 +0800

    release code

--- scripts/train_S4_chair_airplane_car.sh
+++ scripts/train_S4_chair_airplane_car.sh
@@ -1,5 +1,5 @@
 
-python train_generation.py --distribution_type 'multi' \
+python train.py --distribution_type 'multi' \
     --dataroot /path/to/ShapeNetCore.v2.PC15k/ \
     --category chair,airplane,car \
     --experiment_name /path/to/experiments \
